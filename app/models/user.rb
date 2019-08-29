b13443774a57e71d58ec1fd1078eee44c5f681cd
taro.yamata <techyamata@gmail.com>
2019-08-29 07:44:03 +0000

    associate

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
     has_many :followings, through: :relationships, source: :follow
     has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
     has_many :followers, through: :reverses_of_relationship, source: :user
+    has_many :favorites
+    has_many :likings, through: :favorites , source: :micropost
+    
     
     def follow(other_user)
     unless self == other_user
@@ -29,5 +32,17 @@ class User < ApplicationRecord
     end
     def feed_microposts
         Micropost.where(user_id: self.following_ids + [self.id])
+    end
+    def favorite(other_micropost)
+      unless self == other_micropost
+        self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+      end
+    end
+    def unfavorite(other_micropost)
+      favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+      favorite.destroy if favorite
+    end
+    def liking?(other_micropost)
+      self.likings.include?(other_micropost)
     end
 end
